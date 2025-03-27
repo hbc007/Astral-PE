e38fa038611586c9c5507fb47382d9cf990bb724
DosX <admin@kay-software.ru>
2025-03-27 11:13:00 +0300

    Some changes that do not affect functionality

--- source/modules/InvalidPeImageException.cs
+++ source/modules/InvalidPeImageException.cs
@@ -1,3 +1,32 @@
+/*
+ * This file is part of the Astral-PE project.
+ * Copyright (c) 2025 DosX-dev. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Astral-PE is a low-level post-compilation PE header mutator (obfuscator) for native
+ * Windows x86/x64 binaries. It modifies structural metadata while preserving execution integrity.
+ *
+ * For source code, updates, and documentation, visit:
+ * https://github.com/DosX-dev/Astral-PE
+ */
+
 using System;
 
 namespace AstralPE.Obfuscator {
--- source/modules/PeMutator_modules/DebugStripper.cs
+++ source/modules/PeMutator_modules/DebugStripper.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/*
+ * This file is part of the Astral-PE project.
+ * Copyright (c) 2025 DosX-dev. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Astral-PE is a low-level post-compilation PE header mutator (obfuscator) for native
+ * Windows x86/x64 binaries. It modifies structural metadata while preserving execution integrity.
+ *
+ * For source code, updates, and documentation, visit:
+ * https://github.com/DosX-dev/Astral-PE
+ */
+
+using System;
 using System.Linq;
 using System.Text;
 using PeNet;
@@ -25,7 +54,7 @@ namespace AstralPE.Obfuscator.Modules {
             if (optStart + 0x60 + 16 * 8 > raw.Length)
                 throw new Exception("Optional Header is corrupted or incomplete.");
 
-            // Clear the Debug Directory if present
+            // 1. Clear the Debug Directory if present
             if (pe.ImageDebugDirectory != null && pe.ImageDebugDirectory.Any()) {
                 foreach (ImageDebugDirectory? dbg in pe.ImageDebugDirectory) {
                     int dbgOffset = (int)dbg.PointerToRawData;
